1019ab9b1f826868c2ec41fb2738d3780b91d59d
Naoya Fujio <naofudiwo@gmail.com>
2020-06-05 09:25:51 +0000

    do migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_024157) do
+ActiveRecord::Schema.define(version: 2020_06_05_092438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_024157) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
